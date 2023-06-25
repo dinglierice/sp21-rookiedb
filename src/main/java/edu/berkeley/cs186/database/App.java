641cada439f489d3cc5c97ecf0ab09e7b1fee747
dinglierice <dinglie1997@163.com>
2023-06-25 17:05:36 +0800

    proj01 done

---
+++ src/main/java/edu/berkeley/cs186/database/App.java
@@ -0,0 +1,12 @@
+package edu.berkeley.cs186.database;
+
+/**
+ * @description：TODO
+ * @author：dinglie
+ * @date：2023/6/25 17:05
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
